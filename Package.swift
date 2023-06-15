558a552a7b46cc59202520983e00b8f16ef3ea13
Edon <edon@valdman.works>
2023-06-15 16:02:54 -0500

    Update Package.swift
    Added specific platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIPHPicker",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v13),
+        .macCatalyst(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
