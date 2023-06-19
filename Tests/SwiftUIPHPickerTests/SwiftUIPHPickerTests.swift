dd68a80dd9cbaf6afa899e7209a0f7b16c5c4c8d
Edon <edon@valdman.works>
2023-06-19 09:54:19 -0500

    Update SwiftUIPHPickerTests.swift
    Commented-out invalid boilerplate test.

--- Tests/SwiftUIPHPickerTests/SwiftUIPHPickerTests.swift
+++ Tests/SwiftUIPHPickerTests/SwiftUIPHPickerTests.swift
@@ -6,6 +6,6 @@ final class SwiftUIPHPickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUIPHPicker().text, "Hello, World!")
+//        XCTAssertEqual(SwiftUIPHPicker().text, "Hello, World!")
     }
 }
