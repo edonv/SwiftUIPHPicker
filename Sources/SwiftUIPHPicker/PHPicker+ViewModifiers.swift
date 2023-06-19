d155c3a9447a0632a3d6d7552d5e4f9b5e5e865c
Edon <edon@valdman.works>
2023-06-19 10:51:33 -0500

    Update PHPicker+ViewModifiers.swift
    Removed view modifiers, as they're not really needed anymore (since updated to initializers). Might add other modifiers that directly edit properties on the configuration (such as `filter` and `selectedLimit`).

--- Sources/SwiftUIPHPicker/PHPicker+ViewModifiers.swift
+++ Sources/SwiftUIPHPicker/PHPicker+ViewModifiers.swift
@@ -9,19 +9,5 @@ import PhotosUI
 import SwiftUI
 
 extension View where Self == PHPicker {
-    /// Allows for configuration of the ``PHPicker``.
-    /// - Parameter configuration: A new [`PHPickerConfiguration`](https://developer.apple.com/documentation/photokit/phpickerconfiguration).
-    /// - Returns: A new ``PHPicker`` with the provided configuration.
-    public func withConfiguration(_ configuration: PHPickerConfiguration) -> PHPicker {
-        var newView = self
-        newView.setConfiguration(configuration)
-        return newView
-    }
     
-    /// Allows for configuration of the ``PHPicker``.
-    /// - Parameter configurationHandler: A closure for creating a [`PHPickerConfiguration`](https://developer.apple.com/documentation/photokit/phpickerconfiguration).
-    /// - Returns: A new ``PHPicker`` with the provided configuration.
-    public func withConfiguration(_ configurationHandler: () -> PHPickerConfiguration) -> PHPicker {
-        self.withConfiguration(configurationHandler())
-    }
 }
