596610d012cadda7e61fe49076b60fffb06befdc
Edon <edon@valdman.works>
2023-06-19 14:56:58 -0500

    Update PHPicker+ViewModifiers.swift
    Added initial versions of new view modifiers that allow for convenient setting of `configuration` fields. Might remove in the future.

--- Sources/SwiftUIPHPicker/PHPicker+ViewModifiers.swift
+++ Sources/SwiftUIPHPicker/PHPicker+ViewModifiers.swift
@@ -9,5 +9,34 @@ import PhotosUI
 import SwiftUI
 
 extension View where Self == PHPicker {
+    @available(iOS 15, macCatalyst 15, macOS 13, *)
+    public func updatingConfiguration(
+        filter: PHPickerFilter? = nil,
+        selectionLimit: Int = 0,
+        selection: PHPickerConfiguration.Selection = .default
+    ) -> PHPicker {
+        var newView = self
+        var newConfig = newView.configuration
+        
+        newConfig.filter = filter
+        newConfig.selectionLimit = selectionLimit
+        newConfig.selection = selection
+        
+        newView.setConfiguration(newConfig)
+        return newView
+    }
     
+    public func updatingConfiguration(
+        filter: PHPickerFilter? = nil,
+        selectionLimit: Int = 0
+    ) -> PHPicker {
+        var newView = self
+        var newConfig = newView.configuration
+        
+        newConfig.filter = filter
+        newConfig.selectionLimit = selectionLimit
+        
+        newView.setConfiguration(newConfig)
+        return newView
+    }
 }
