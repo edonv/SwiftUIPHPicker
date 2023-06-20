d14b2bd5b121f39a3c2f001bced9c3df2e2547b5
Edon <edon@valdman.works>
2023-06-20 14:01:33 -0500

    Update NSItemProvider+Extensions.swift
    Added an `async` overload of `NSItemProvider`'s `loadObject(ofClass:completionHandler:)` function.

--- Sources/SwiftUIPHPicker/NSItemProvider+Extensions.swift
+++ Sources/SwiftUIPHPicker/NSItemProvider+Extensions.swift
@@ -21,4 +21,18 @@ extension NSItemProvider {
             }
         }
     }
+    
+    public func loadObject<T>(
+        ofClass aClass: T.Type
+    ) async throws -> T where T : NSItemProviderReading {
+        return try await withCheckedThrowingContinuation { continuation in
+            loadObject(ofClass: aClass.self) { object, error in
+                if let object {
+                    continuation.resume(returning: object as! T)
+                } else if let error {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
 }
