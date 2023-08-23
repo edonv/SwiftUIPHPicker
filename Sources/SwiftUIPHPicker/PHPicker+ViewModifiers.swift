37611fd808bb8d79e19eacc809ce9e414f85d7b7
Edon <edon@valdman.works>
2023-08-23 14:23:18 -0500

    Update PHPicker+ViewModifiers.swift
    Removed view modifiers that include the sheet presentation.

--- Sources/SwiftUIPHPicker/PHPicker+ViewModifiers.swift
+++ Sources/SwiftUIPHPicker/PHPicker+ViewModifiers.swift
@@ -8,51 +8,6 @@
 import PhotosUI
 import SwiftUI
 
-extension View {
-    public func phPicker(
-        isPresented: Binding<Bool>,
-        selection: Binding<[PHSelectedObject]>,
-        maxSelectionCount: Int? = nil,
-        matching filter: PHPickerFilter? = nil,
-        preferredAssetRepresentationMode: PHPickerConfiguration.AssetRepresentationMode = .automatic,
-        keepLivePhotosIntact: Bool = true,
-        photoLibrary: PHPhotoLibrary,
-        onDismiss: (() -> Void)? = nil
-    ) -> some View {
-        self
-            .sheet(isPresented: isPresented, onDismiss: onDismiss) {
-                PHPicker(selections: selection, keepLivePhotosIntact: keepLivePhotosIntact, photoLibrary: photoLibrary) { config in
-                    config.selectionLimit = maxSelectionCount ?? 0
-                    config.filter = filter
-                    config.preferredAssetRepresentationMode = preferredAssetRepresentationMode
-                }
-            }
-    }
-    
-    @available(iOS 15, macCatalyst 15, macOS 13, *)
-    public func phPicker(
-        isPresented: Binding<Bool>,
-        selection: Binding<[PHSelectedObject]>,
-        maxSelectionCount: Int? = nil,
-        selectionBehavior: PHPickerConfiguration.Selection = .default,
-        matching filter: PHPickerFilter? = nil,
-        preferredAssetRepresentationMode: PHPickerConfiguration.AssetRepresentationMode = .automatic,
-        keepLivePhotosIntact: Bool = true,
-        photoLibrary: PHPhotoLibrary,
-        onDismiss: (() -> Void)? = nil
-    ) -> some View {
-        self
-            .sheet(isPresented: isPresented, onDismiss: onDismiss) {
-                PHPicker(selections: selection, keepLivePhotosIntact: keepLivePhotosIntact, photoLibrary: photoLibrary) { config in
-                    config.selectionLimit = maxSelectionCount ?? 0
-                    config.selection = selectionBehavior
-                    config.filter = filter
-                    config.preferredAssetRepresentationMode = preferredAssetRepresentationMode
-                }
-            }
-    }
-}
-
 extension PHPicker {
     /// Sets the maximum number of selections the user can make.
     ///
